7df7efde6cd16fbaa6430e251d046c7be8476350
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2021-09-14 22:03:39 -0400

    Calculator Bonus Features

--- lesson2/calculator.rb
+++ lesson2/calculator.rb
@@ -1,80 +1,64 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+LANGUAGE = 'en'
 
-def prompt(message)
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def prompt(key, replacement = 'to_change')
+message = format(messages(key, LANGUAGE), var: replacement)
   Kernel.puts("=> #{message}")
 end
 
+def get_name
+  name = ''
+  loop do
+    name = Kernel.gets().chomp()
+
+    if name.split == []
+      prompt(:valid_name)
+    else
+      break
+    end
+  end
+
+  name
+end
+
+def get_number
+  number = ''
+  loop do
+
+    number = Kernel.gets().chomp()
+
+    if valid_number?(number)
+      number = number.to_f
+      break
+    else
+      prompt(:valid_number)
+    end
+  end
+
+  number
+end
+
+# Bonus Feature 1: Better integer validation
+def valid_integer?(num)
+  num.to_i().to_s() == num
+end
+
+# Float validation for Bonus Feature 2
+def valid_float?(num)
+  num.to_f().to_s() == num
+end
+
+# Bonus Feature 2: Number validation
 def valid_number?(num)
-  num.to_i() != 0
+  valid_integer?(num) || valid_float?(num)
 end
 
-def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
-end
-
-prompt("Welcome to Calculator! Enter your name:")
-
-name = ''
-loop do
-  name = Kernel.gets().chomp()
-
-  if name.empty?()
-    prompt("Make sure to use a vaild name.")
-  else
-    break
-  end
-end
-
-prompt("Hi, #{name}")
-
-loop do
-  number1 = ''
-  number2 = ''
-
-  loop do
-    prompt("What's the first number?")
-    number1 = Kernel.gets().chomp()
-
-    if valid_number?(number1)
-      break
-    else
-      prompt("Hmm... that doesn't look like a valid number")
-    end
-  end
-
-  loop do
-    prompt("What's the second number?")
-    number2 = Kernel.gets().chomp()
-
-    if valid_number?(number2)
-      break
-    else
-      prompt("Hmm... that doesn't look like a valid number")
-    end
-  end
-
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  prompt(operator_prompt)
-
+def get_operator()
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
@@ -82,28 +66,69 @@ loop do
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3, or 4")
+      prompt(messages(:valid_choice))
     end
   end
+  operator
+end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
+# Bonus Feature 3: Improve this method
+def operation_to_message(op)
+  message = case op
+            when '1'
+              'Adding'
+            when '2'
+              'Subtracting'
+            when '3'
+              'Multiplying'
+            when '4'
+              'Dividing'
+            end
+  message
+end
+
+Kernel.system("clear")
+
+prompt(:welcome)
+
+name = get_name()
+
+prompt(:hello, name)
+Kernel.sleep(1)
+
+loop do
+  Kernel.system("clear")
+  
+  prompt(messages(:first_number))
+  number1 = get_number()
+
+  prompt(messages(:second_number))
+  number2 = get_number()
+
+  prompt(messages(:operator_prompt))
+
+  operator = get_operator()
+
+  Kernel.system("clear")
+
+  prompt(operation_to_message(operator) + messages(:operation)")
 
   result = case operator
            when '1'
-             number1.to_i() + number2.to_i()
+             number1 + number2
            when '2'
-             number1.to_i() - number2.to_i()
+             number1 - number2
            when '3'
-             number1.to_i() * number2.to_i()
+             number1 * number2
            when '4'
-             number1.to_f() / number2.to_f()
+             number1 / number2
            end
 
   prompt("The result is: #{result}")
 
-  prompt("Do you want to perform another calculation? (Y to calculated again)")
+  prompt(MESSAGES[:go_again])
   answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase() == 'y' || answer.downcase() == 'yes'
 end
 
-prompt("Thank you for using the calculator. Goodbye!")
+prompt(MESSAGES[:goodbye])
