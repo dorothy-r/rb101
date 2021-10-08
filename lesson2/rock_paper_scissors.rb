4a9d43d1bd3f86928cef3a3817425b44e84c9d6a
Dorothy Rhodes <70044671+dorothy-r@users.noreply.github.com>
2021-10-08 13:57:16 -0400

    Refactor RPSLS

--- lesson2/rock_paper_scissors.rb
+++ lesson2/rock_paper_scissors.rb
@@ -7,7 +7,7 @@ VALID_CHOICES = { 'rock' =>
                     defeats: ['rock', 'spock'],
                     verbs: ['covers', 'disproves'] },
                   'scissors' =>
-                  { alias: 's',
+                  { alias: 'sc',
                     defeats: ['paper', 'lizard'],
                     verbs: ['cut', 'decapitate'] },
                   'lizard' =>
@@ -25,6 +25,20 @@ WELCOME = <<~MSG
   Welcome to Rock, Paper, Scissors, Lizard, Spock!
   ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
+  Would you like to review the rules (y/n)?
+  MSG
+
+BEGIN_GAME = <<~MSG
+  The computer is challenging you to a game.
+
+  Win 3 rounds and you will be named the grand winner!
+
+  Press 'enter' when you are ready to begin. Good luck!
+  MSG
+
+RULES = <<~MSG
+  ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+
   The Rules:
   Scissors cut Paper      Paper covers Rock
   Rock crushes Lizard     Lizard poisons Spock
@@ -32,15 +46,13 @@ WELCOME = <<~MSG
   Lizard eats Paper       Paper disproves Spock
   Spock vaporizes Rock    Rock crushes Scissors
 
+  
   You can enter your choice's full name or use a shortcut:
-  'r' for rock, 'p' for paper, 's' for scissors, 
+  'r' for rock, 'p' for paper, 'sc' for scissors, 
   'l' for lizard, 'sp' for spock
 
-  Got it? Good!
-  The computer is challenging you to a game.
-  The first player to win three rounds will be named the grand winner!
-  Good luck!
-  To continue, type any key and press enter.
+ 
+  Got it? Press 'enter' to continue.
 
   ~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
   MSG
@@ -52,13 +64,27 @@ end
 def introduction
   system 'clear'
   puts WELCOME
+  answer = ''
+  loop do
+    answer = gets.strip.downcase
+    break if %w(y yes n no).include? answer
+    prompt("Please enter '(y)es' or '(n)o'")
+  end
+  if answer == 'y' || answer == 'yes'
+    puts RULES
+    gets
+  end
+  system 'clear'
+  puts BEGIN_GAME
+  gets
 end
 
 def get_choice
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.keys.join(', ')}")
-    choice = gets.chomp.downcase
+    prompt("Choose one: ")
+    prompt("(r)ock, (p)aper, (sc)issors, (l)izard, (sp)ock")
+    choice = gets.strip.downcase
     choice = convert_input(choice)
     break if valid_choice?(choice)
     prompt("That's not a valid choice.")
@@ -70,8 +96,6 @@ def convert_input(choice)
   VALID_CHOICES.each do |_k, v|
     if v.value?(choice)
       choice = VALID_CHOICES.key(v)
-    else
-      choice
     end
   end
   choice
@@ -101,7 +125,7 @@ def display_choices(player, computer)
   prompt("Computer chose: #{computer}")
 end
 
-def display_sentence(winner, player, computer)
+def display_results(winner, player, computer)
   puts "\n"
   if winner == :player
     winner = player
@@ -113,12 +137,12 @@ def display_sentence(winner, player, computer)
     prompt("You chose the same thing.")
     return
   end
-  i = VALID_CHOICES[winner][:defeats].index(loser)
-  verb = VALID_CHOICES[winner][:verbs][i]
+  index = VALID_CHOICES[winner][:defeats].index(loser)
+  verb = VALID_CHOICES[winner][:verbs][index]
   prompt("*** #{winner.capitalize} #{verb} #{loser}. ***")
 end
 
-def display_results(winner)
+def display_round_winner(winner)
   if winner == :player
     prompt("You won this round!")
   elsif winner == :computer
@@ -138,25 +162,28 @@ def display_scores(scores)
   scores.each do |k, v|
     prompt("#{k.capitalize}: #{v}")
   end
+  puts "\n"
+  prompt("Press 'enter' to continue")
+  gets
 end
 
-def display_winner(scores)
+def display_grand_winner(scores)
   if scores[:player] == WINNING_SCORE
     prompt("You are the grand winner! Congratulations!")
   else
     prompt("The computer is the grand winner! Try again.")
   end
+  puts "\n"
 end
 
 def play_again?
   prompt("Do you want to play again?")
   prompt("If so, enter 'Y'. Enter any other key to exit.")
-  answer = gets.chomp
-  answer.downcase == 'y'
+  answer = gets.strip
+  answer.downcase == 'y' || answer.downcase == 'yes'
 end
 
 introduction
-gets
 loop do
   system 'clear'
   scores = { player: 0, computer: 0 }
@@ -167,19 +194,18 @@ loop do
     round_winner = get_winner(player_choice, computer_choice)
     display_choices(player_choice, computer_choice)
     sleep 0.5
-    display_sentence(round_winner, player_choice, computer_choice)
-    display_results(round_winner)
-    sleep 1
+    display_results(round_winner, player_choice, computer_choice)
+    display_round_winner(round_winner)
+    sleep 0.5
 
     increment_score(round_winner, scores) if round_winner
     display_scores(scores)
-    sleep 1.5
     system 'clear'
 
     break if scores.value?(WINNING_SCORE)
   end
 
-  display_winner(scores)
+  display_grand_winner(scores)
 
   break unless play_again?
 end
